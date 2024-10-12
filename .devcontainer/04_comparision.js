a9e416a9194d1536f39a70033780cf9e756e55d5
Ujwal Rakesh Pandey <135531068+ujwal7718@users.noreply.github.com>
2024-10-12 18:40:27 +0000

    done

---
+++ .devcontainer/04_comparision.js
@@ -0,0 +1,9 @@
+// console.log("2" > 1);
+// console.log(null >0);
+// console.log(null >=0);
+
+console.log(undefined < 0);
+console.log(undefined >=0);
+
+// === (Strict Check, it also checks the datatype)
+console.log("2" === 2);
